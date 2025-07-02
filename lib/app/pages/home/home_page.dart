658e8884705eb96583392227e6fd4ee8eac64fa6
mancj <mansur.ekaev@lab80.io>
2025-07-03 00:32:43 +0300

    Refactor app template

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -1,10 +1,7 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:get/get.dart';
 import 'package:my_app_template/app/resources/ui_resources.dart';
-import 'package:my_app_template/app/widgets/margin.dart';
-import 'package:my_app_template/app/widgets/app_gesture_detector.dart';
-import 'package:my_app_template/app/widgets/ui_kit/squircle_container.dart';
+import 'package:my_app_template/app/widgets/page_title.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import 'home_page_controller.dart';
 
@@ -18,63 +15,14 @@ class HomePage extends GetView<HomeController> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       backgroundColor: UIColors.pageBackground,
       body: SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(16, 24, 16, 0),
-              child: RichText(
-                textAlign: TextAlign.start,
-                text: const TextSpan(children: [
-                  TextSpan(
-                    text: 'Арабский\n',
-                    style: UITextStyles.pageTitleSemibold,
-                  ),
-                  TextSpan(
-                    text: 'словарь',
-                    style: UITextStyles.pageTitle,
-                  ),
-                ]),
-              ),
-            ),
-            _searchBar(),
+            PageTitle('Flutter app'),
           ],
-        ),
-      ),
-    );
-  }
-
-  Container _searchBar() {
-    return Container(
-      width: double.infinity,
-      margin: const EdgeInsets.all(16),
-      height: 48,
-      child: SquircleContainer(
-        child: AppGestureDetector(
-          child: Container(
-            padding: const EdgeInsets.only(left: 12, right: 16),
-            child: Row(
-              children: [
-                SvgPicture.asset(UISVGAssets.search),
-                const Margin.horizontal(16),
-                RichText(
-                  text: const TextSpan(
-                    style: UITextStyles.regular17,
-                    children: [
-                      TextSpan(
-                        text: 'Нажмите',
-                        style: UITextStyles.semibold17,
-                      ),
-                      TextSpan(text: ' для поиска'),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
         ),
       ),
     );
--- lib/app/widgets/page_title.dart
+++ lib/app/widgets/page_title.dart
@@ -3,14 +3,18 @@ import 'package:flutter/widgets.dart';
 
 class PageTitle extends StatelessWidget {
   final String title;
+
   const PageTitle(this.title, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Text(
-      "Metal Detector",
-      style: UITextStyles.pageTitle,
-      textAlign: TextAlign.center,
+    return const Padding(
+      padding: EdgeInsets.all(16),
+      child: Text(
+        "Flutter App",
+        style: UITextStyles.pageTitle,
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
